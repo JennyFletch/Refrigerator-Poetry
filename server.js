084a570f654be0bc987d108ebdc5cb1737f5ee97
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-06-16 13:54:40 -0600

    CORS adjustments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
@@ -9,6 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
